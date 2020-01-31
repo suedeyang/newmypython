a43eeddbefb84778b60b857c42c955e1b22bff46
suedeyang <suedeyang@gmail.com>
2020-01-31 14:54:52 +0800

    安裝pip一定要有的py 最重要的是pytube 無法使用

---
+++ 8-13_youtube簡易download.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 11:17:55 2020
+
+@author: suedeyang
+"""
+
+from pytube import YouTube
+yt=YouTube(url='https://www.youtube.com/watch?v=JwBXgJeqeOs')
+

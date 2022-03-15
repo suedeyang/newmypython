825e0cceff6dae025a0127961f96d89a9afb2156
suedeyang <suedeyang@gmail.com>
2022-03-15 15:56:37 +0800

    新增自動按確定autoclick.py
    可以自動按確定

---
+++ pyautogui/autoclick.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+while True:
+    button7location = pyautogui.locateOnScreen('sure.jpg', confidence=0.8)
+    if button7location:
+        button7point=pyautogui.center(button7location)
+        pyautogui.click(button7location)
+    #time.sleep(5)
+    #pyautogui.scroll(3)
+    pyautogui.PAUSE=100

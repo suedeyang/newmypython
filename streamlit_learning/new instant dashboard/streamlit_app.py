fc4a710bd19bcb61db591cbf79829c025e7a4f0a
suedeyang <suedeyang@gmail.com>
2022-04-06 21:11:48 +0800

    新資訊看板

---
+++ streamlit_learning/new instant dashboard/streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plost
+from PIL import Image
+
+# Page setting
+st.set_page_config(layout="wide")
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Row A
+a1, a2, a3 = st.columns(3)
+#a1.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
+a2.metric("Wind", "9 mph", "-8%")
+a3.metric("Humidity", "86%", "4%")
+
+# Row B
+b1, b2, b3, b4 = st.columns(4)
+b1.metric("Temperature", "70 °F", "1.2 °F")
+b2.metric("Wind", "9 mph", "-8%")
+b3.metric("Humidity", "86%", "4%")
+b4.metric("Humidity", "86%", "4%")
+

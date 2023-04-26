144211cd576024390d945af1f080930cff781c09
suedeyang <suedeyang@gmail.com>
2023-04-26 10:00:13 +0800

    Update auto_check_KH.py

--- 市內介聘查詢/auto_check_KH.py
+++ 市內介聘查詢/auto_check_KH.py
@@ -1,17 +1,20 @@
 #-*-coding:utf-8-*-
 from selenium import webdriver
 import time
+import requests
 url = "https://portal.kh.edu.tw/"
 url2 = "https://portal.kh.edu.tw/redirect.php?func=kh:exc"
 url3 = "https://exc.kh.edu.tw/p/tea/school_apply_no_i.jsp?school="
 
-#讀取高雄市國小學校代碼
-f = open('school_no.txt', 'r')
-school_no = []
-for line in f:
-    school_no.append(line)
-f.close()
 
+school_no = []
+#讀取高雄市國小學校代碼
+response = requests.get("https://www2.lhps.kh.edu.tw/school_no.txt").text.split('\n')
+for i in response:
+    school_no.append(i)
+
+print(f'共有 {len(school_no)} 間高市國小要讀取資料\n接下來會跳轉到資訊服務入口網，請"立刻"輸入帳號、密碼、驗證碼後按"登入"等待程式啟動\n \n提醒您!本程式僅能查詢「高市某校有多少人想調入貴校」\n執行過程中請勿關閉跳動的瀏覽器視窗 \n執行完畢會出現一個「檢查結果」的文字檔')
+time.sleep(20)
 
 driver=webdriver.Chrome()
 driver.get(url)
@@ -28,5 +31,5 @@ for i in school_no:
     time.sleep(0.5)
     result=driver.find_element("xpath",'/html/body/div[3]/p/font').text
     if result[-3] != "0":
-        with open('appendSomething.txt', 'a') as f:
+        with open('檢查結果.txt', 'a') as f:
             f.write(f'{result}\n')

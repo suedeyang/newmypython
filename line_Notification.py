13969399f019134e5a5638975ed00ce4143131b3
suedeyang <suedeyang@gmail.com>
2023-10-31 09:06:15 +0800

    Create line_Notification.py

---
+++ line_Notification.py
@@ -0,0 +1,22 @@
+import requests
+# https://steam.oxxostudio.tw/category/python/spider/line-notify.html
+# https://notify-bot.line.me/doc/en/
+
+
+token = ''
+headers={
+    "Authorization": "Bearer " + token   #Bearer後要有一個空白
+    }
+
+# https://developers.line.biz/en/docs/messaging-api/sticker-list/
+
+
+data={
+    'message':'測試一下',
+    'stickerPackageId':'446',
+    'stickerId': '1990',
+    'notificationDisabled':'True'
+}
+
+response_result=requests.post("https://notify-api.line.me/api/notify",headers=headers,data=data)
+print(response_result)
